ac09fd14f60723abd33b2f57fde0e32b45c2ca0e
supraja Bommisetty <suprajabommisetty@rarecrew.com>
2025-10-16 09:55:34 +0530

    LoadDM

---
+++ eCommerceDM/Fact/Tables/factSales.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [Fact].[factSales] (
+    [saleKey]      INT          IDENTITY (1001, 1) NOT NULL,
+    [orderId]      VARCHAR (10) NULL,
+    [dateKey]      INT          NOT NULL,
+    [customerKey]  INT          NOT NULL,
+    [GeographyKey] INT          NOT NULL,
+    [productKey]   INT          NOT NULL,
+    [Amount]       INT          NULL,
+    [Profit]       SMALLINT     NULL,
+    [Quantity]     TINYINT      NULL,
+    PRIMARY KEY CLUSTERED ([saleKey] ASC),
+    CONSTRAINT [FK_factSales_dimCustomer_customerKey] FOREIGN KEY ([customerKey]) REFERENCES [Dim].[dimCustomer] ([customerKey]),
+    CONSTRAINT [FK_factSales_dimDate_dateKey] FOREIGN KEY ([dateKey]) REFERENCES [Dim].[dimDate] ([dateKey]),
+    CONSTRAINT [FK_factSales_dimGeography_GeographyKey] FOREIGN KEY ([GeographyKey]) REFERENCES [Dim].[dimGeography] ([GeographyKey]),
+    CONSTRAINT [FK_factSales_dimProduct_ProductKey] FOREIGN KEY ([productKey]) REFERENCES [Dim].[dimProduct] ([ProductKey])
+);
+
---
+++ eCommerceDM/dbo/Stored Procedures/usp_PopulateDimDate.sql
@@ -0,0 +1,23 @@
+﻿create   procedure dbo.usp_PopulateDimDate
+As 
+ begin
+	set nocount on;
+
+	INSERT INTO Dim.dimDate (
+		dateKey,
+		fullDate,
+		saleYear,
+		saleMonth,
+		saleMonthName
+		)
+        SELECT distinct
+            CONVERT(INT, FORMAT(OrderDate, 'yyyyMMdd')) AS dateKey,
+            OrderDate as fullDate,
+			YEAR(OrderDate) AS saleYear,
+            MONTH(OrderDate) AS saleMonth,
+            DATENAME(MONTH, OrderDate) AS saleMonthName
+            
+        FROM eCommerceSalesDMStaging.sales.listOfOrders
+        WHERE  CONVERT(INT, FORMAT(OrderDate, 'yyyyMMdd')) 
+            NOT IN (SELECT dateKey FROM Dim.dimDate);
+  End
